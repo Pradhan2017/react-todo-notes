13797650410b0b0e2f945bda6770a6812bde924e
Pradhan2017 <cp245982gmail.com>
2018-03-01 21:27:18 +0530

    First commit

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyCva5PvvAIxHgfKBGB3DGeCg1OiRFA6EmU",
+    authDomain: "react-notes-b0860.firebaseapp.com",
+    databaseURL: "https://react-notes-b0860.firebaseio.com",
+    projectId: "react-notes-b0860",
+    storageBucket: "react-notes-b0860.appspot.com",
+    messagingSenderId: "94601190391"
+  };
